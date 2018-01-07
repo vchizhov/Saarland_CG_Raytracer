c509fedc23fa9a58c96734d6998e4e25700a2b35
LAP-05-47\Administrator <vassillenchizhov@gmail.com>
2018-01-07 13:20:24 +0100

    initial stuff for a6

--- main/main.cpp
+++ main/main.cpp
@@ -6,10 +6,14 @@ void a_indexing();
 void a_instancing();
 void a_lighting();
 void a_materials();
+void a_distributed();
+void a_textures();
+void a_mappers();
 
 int main(int argc, char* argv[])
 {
-    a_lighting();
-	a_materials();
+    a_textures();
+	// a_mappers();
+	// a_distributed();
     return 0;
 }
